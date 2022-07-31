8d1d56f20f8668b3f109a79b0196eae6ea8c44f5
RenatiusvanSander / R. Meier <remad@web.de>
2022-07-31 12:58:37 +0200

    master: math tasks isPrime

---
+++ src/main/java/edu/remad/javachallenges/mathtasks/PrimeNumbers.java
@@ -0,0 +1,23 @@
+package edu.remad.javachallenges.mathtasks;
+
+/**
+ * Processes prime numbers for a number.
+ */
+public class PrimeNumbers {
+
+  /**
+   * validates a prime number.
+   *
+   * @param potentiallyPrime a number that is potentially prime number candidate
+   * @return true / false for is a prime number
+   */
+  public static boolean isPrime(final int potentiallyPrime) {
+    for(int i = 2; i < potentiallyPrime / 2; i++) {
+      if(potentiallyPrime % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
