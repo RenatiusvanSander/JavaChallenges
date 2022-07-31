67e41f94bc1205d390a60ff5090e3b8e2abe2aa1
RenatiusvanSander / R. Meier <remad@web.de>
2022-07-31 12:49:37 +0200

    master: math tasks find proper divisorsd with test java doc

--- src/test/java/edu/remad/javachallenge/mathtasks/ProperDivisorFinderTest.java
+++ src/test/java/edu/remad/javachallenge/mathtasks/ProperDivisorFinderTest.java
@@ -1,19 +1,27 @@
 package edu.remad.javachallenge.mathtasks;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 import edu.remad.javachallenges.mathtasks.ProperDivisorFinder;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
+/**
+ * JUnit test for {@link ProperDivisorFinder}
+ */
 public class ProperDivisorFinderTest {
 
+  /**
+   * Tests find divisors for 100.
+   */
   @Test
-  public void testFindProperDivisors() {
-    int numberValue = 100;
+  public void testFindProperDivisorsShouldContainExpectedDivisors() {
+    Integer[] expectedDivisors = new Integer[]{1, 2, 4, 5, 10, 20, 25, 50};
 
-    List<Integer> actualDivisors = ProperDivisorFinder.findProperDivisors(numberValue);
+    int numberToFindDivisors = 100;
+    List<Integer> actualDivisors = ProperDivisorFinder.findProperDivisors(numberToFindDivisors);
 
-    assertEquals(8, actualDivisors.size(), "");
+    assertThat(actualDivisors, containsInAnyOrder(expectedDivisors));
   }
 }
